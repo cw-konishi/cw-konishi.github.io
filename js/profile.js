6c73994ec8c140cc6b9ea172f1b55741e42c58be
Kota KONISHI <kota.konishi.ey@nttcom.co.jp>
2026-02-07 15:50:24 +0900

    モバイルデバイス向けのBlob数を5000から2500に減少

--- js/profile.js
+++ js/profile.js
@@ -8,7 +8,7 @@ const SETTINGS = {
 };
 
 const blobs = [];
-const BLOB_COUNT = window.innerWidth <= 768 ? 5000 : 400;
+const BLOB_COUNT = window.innerWidth <= 768 ? 2500 : 400;
 
 const pointer = {
   x: 0,
